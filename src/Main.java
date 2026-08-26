63030e4f42e1fd7c62f1ec8a613d7e1c576ebed9
om22-max <rathoreom686@gmail.com>
2026-08-26 23:49:19 +0530

    Added today's Java programs

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+//void main() {
+//    System.out.println("Hello World");
+//}
+
+
+//void main() {
+//    String name = "om" ;
+//    System.out.println("name");
+//}
+void main () {
+    String om = "om";
+    String i am Om = "Rathore";
+
+    System.out.println(om);
+    System.out.println(Om);
+}
+
