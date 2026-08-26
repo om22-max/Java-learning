0e0f219e7218e4f750ce258e36f0a7a3d6d663b2
om22-max <om2845909@gmail.com>
2026-08-27 00:32:15 +0530

    day 2 Variables in jav

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+//VARIABLES IN JAVA
+//
+//we have some rules to follow while writing a java program .the set of rules is called syntax. like we follow (grammer) some rules while speak English .
+//
+//VARIABLES = Variable is a container that store values. the value can be change during execution time in the program .
+//
+//EXAMPLE OF VARIABLE
+//int number = 10
+//
+//int = data type
+//number = variable
+//10 = value that store
+
+//RULES OF WRITING VARIABLES
+//
+//1. A variable can't start  with a digit or number. EXAMPLE
+
+//void main () {
+//    String om = "om";
+//    String 1Om = "Rathore";
+//    System.out.println(om);
+//    System.out.println(Om);
+//}
+
+//2.Name case is sensitive Om or om are different . EXAMPLE
+
+void main () {
+    String om = "om";
+    String 1Om = "Rathore";
+    System.out.println(om);
+    System.out.println(Om);
+}
+
+//3.White spaces are not allowed in java . EXAMPLE
+
+void main () {
+    String om = "om";
+    String i am Om = "Rathore";
+
+    System.out.println(om);
+    System.out.println(Om);
+}
