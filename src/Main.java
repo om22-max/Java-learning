2a877474dd9e3a9442079b9e1daffcf8577ca61a
om22-max <om2845909@gmail.com>
2026-08-28 23:57:41 +0530

    Update Calculator program

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,10 @@
-//VARIABLES IN JAVA
-//
-//we have some rules to follow while writing a java program .the set of rules is called syntax. like we follow (grammer) some rules while speak English .
-//
-//VARIABLES = Variable is a container that store values. the value can be change during execution time in the program .
-//
-//EXAMPLE OF VARIABLE
-//int number = 10
-//
-//int = data type
-//number = variable
-//10 = value that store
+class Calculator {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
 
-//RULES OF WRITING VARIABLES
-//
-//1. A variable can't start  with a digit or number. EXAMPLE
-
-//void main () {
-//    String om = "om";
-//    String 1Om = "Rathore";
-//    System.out.println(om);
-//    System.out.println(Om);
-//}
-
-//2.Name case is sensitive Om or om are different . EXAMPLE
-
-void main () {
-    String om = "om";
-    String 1Om = "Rathore";
-    System.out.println(om);
-    System.out.println(Om);
-}
-
-//3.White spaces are not allowed in java . EXAMPLE
-
-void main () {
-    String om = "om";
-    String i am Om = "Rathore";
-
-    System.out.println(om);
-    System.out.println(Om);
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+    }
 }
