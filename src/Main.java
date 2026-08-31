7350b0b9bd19aab8e6ef7fcff630b08770f92a46
om22-max <rathoreom686@gmail.com>
2026-08-31 17:31:54 +0530

    LITERALS IN JAVA

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,32 @@
-//public class Main {
-//    public static void main (String[] args) {
-//        int members = 1150 ;
-//        byte age = 75 ;
-//        String name = "om" ;
-//        boolean passed = true;
-//        System.out.println(members);
-//        System.out.println(age);
-//        System.out.println(name);
-//        System.out.println(passed);
-//    }
-//}
+//LITERALS IN JAVA
 
-//public class Main {
-//    public static void main(String[] args) {
-//        int num = 111112222;
-//        byte num2 = 111;
-//        short num3 = 1111;
-//        long num4 = 11112222l;  //(L or l its by default)
-//        char letter = 'A';
-//        float num5 = 0.2f;    //(F or f add  because its not by default)
-//        double num6 = 0.2025D; //(d or D its by default)
-//        boolean num7 = true;
-//        System.out.println(num);
-//        System.out.println(num5);
-//    }
-//}
+public class Main {
+    public static void main (String[] args) {
+        int members = 1150 ;
+        byte age = 75 ;
+        String name = "om" ;
+        boolean passed = true;
+        System.out.println(members);
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(passed);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int num = 111112222;
+        byte num2 = 111;
+        short num3 = 1111;
+        long num4 = 11112222l;  //(L or l its by default)
+        char letter = 'A';
+        float num5 = 0.2f;    //(F or f add  because its not by default)
+        double num6 = 0.2025D; //(d or D its by default)
+        boolean num7 = true;
+        System.out.println(num);
+        System.out.println(num5);
+    }
+}
 
 //KEYWORDS IN JAVA EXAMPLE
 
