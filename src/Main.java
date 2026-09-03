d3d40fffb4ab01bec41cc99b3f9877f09efa615a
om22-max <rathoreom686@gmail.com>
2026-09-03 12:54:03 +0530

    Taking input from user

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+//Taking input from user
+
+//import java.util.Scanner;
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println("Taking input from user");
+//
+//        Scanner add = new Scanner(System.in);
+//
+//        System.out.println("enter 1st number: ");
+//        int a = add.nextInt();
+//
+//        System.out.println("enter 2nd number: ");
+//        int b = add.nextInt();
+//
+//        int sum = a + b;
+//
+//        System.out.println("sum of two number is: ");
+//        System.out.println(sum);
+//    }
+//}
+
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner name = new Scanner(System.in);
+//        boolean b1 = name.hasNextInt();
+//        System.out.println(b1);
+//    }
+//}
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner name2 = new Scanner(System.in);
+        boolean b1 = name2.hasNextBoolean();
+        System.out.println(b1);
+    }
+}
